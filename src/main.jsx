59b01233573e6f0cfc401d0fec483d469e761c17
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2025-02-12 21:17:40 +0530

    First commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/ClientSlot.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ClientSlot = () => {
+  return (
+    <div>ClientSlot</div>
+  )
+}

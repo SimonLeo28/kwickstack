99f371858104243fd68f19861bdfb9e307c6fab1
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2025-08-19 21:21:10 +0530

    updated navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = ({ setSelectedSection, selectedSection }) => {
         <h2 className="text-2xl font-bold text-blue-900 mb-4 text-center md:text-left">
           Admin Dashboard
         </h2>
-        <div className="space-y-2 flex flex-col">
+        <div className="space-y-3 flex flex-col">
           {["All", "Client Slot Data", "Invoice", "Blog"].map((section) => (
             <button
               key={section}
               onClick={() => setSelectedSection(section)}
-              className={`w-full text-left px-4 py-3 rounded-lg shadow-md font-medium transition-colors ${
+              className={`w-[80%] text-left px-4 py-3 rounded-lg shadow-md font-medium transition-colors ${
                 selectedSection === section ? "bg-[#f0e9cd]" : "bg-white"
               } hover:bg-[#f0e9cd]`}
             >
